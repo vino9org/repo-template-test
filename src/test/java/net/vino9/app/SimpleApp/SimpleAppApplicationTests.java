5b48c0d5c36089b4f26c2eb6433eaf677c53c7ed
Li Lin <guru.lin@gmail.com>
2024-03-17 16:00:34 +0800

    added generated java code

---
+++ src/test/java/net/vino9/app/SimpleApp/SimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vino9.app.SimpleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
